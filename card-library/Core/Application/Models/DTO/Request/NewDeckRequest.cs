42fb23db2a131e74bc50a428c4e371fb9e316329
mikewng <wangzhxn0@gmail.com>
2026-01-29 22:16:22 -0500

    decks service minor implementations

--- card-library/Core/Application/Models/DTO/Request/NewDeckRequest.cs
+++ card-library/Core/Application/Models/DTO/Request/NewDeckRequest.cs
@@ -1,11 +1,16 @@
 ﻿namespace card_library.Core.Application.Models.DTO.Request
 {
+    public class CardInDeck
+    {
+        public Guid CardId { get; set; }
+        public int CardCount { get; set; }
+    }
     public class NewDeckRequest
     {
         public Guid UserId { get; set; }
         public Guid GameId { get; set; }
         public string DeckName { get; set; } = string.Empty;
         public string DeckDescription { get; set; } = string.Empty;
-        public List<Guid> CardIds { get; set; } = new List<Guid>();
+        public List<CardInDeck> CardIds { get; set; } = new List<CardInDeck>();
     }
 }
--- card-library/Core/Application/Services/DecksService.cs
+++ card-library/Core/Application/Services/DecksService.cs
@@ -1,5 +1,6 @@
 ﻿using card_library.Core.Application.Models.DTO.Request;
 using card_library.Core.Application.Models.DTO.Response;
+using card_library.Core.Application.Repository.Contracts;
 using card_library.Core.Application.Services.Contracts;
 using card_library.Core.Utils;
 
@@ -7,14 +8,41 @@ namespace card_library.Core.Application.Services
 {
     public class DecksService : IDecksService
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IDeckRepository _deck;
+        private readonly IFileRepository _filestorage;
+
+        public DecksService(IDeckRepository deckRepository, IUnitOfWork unitOfWork, IFileRepository filestorage)
+        {
+            _deck = deckRepository;
+            _unitOfWork = unitOfWork;
+            _filestorage = filestorage;
+        }
+
         public Task<Result<NewDeckResponse>> CreateDeck(NewDeckRequest newDeckRequest)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Result<DeckResponse>> GetDeckById(Guid deck_id)
+        public async Task<Result<DeckResponse>> GetDeckById(Guid deck_id)
         {
-            throw new NotImplementedException();
+            var deckResult = await _deck.GetById(deck_id);
+            if (deckResult == null)
+            {
+                return Result<DeckResponse>.Fail("Failed to get deck of associated ID.");
+            }
+
+            // Call S3 to get File
+
+            DeckResponse deckResponse = new DeckResponse
+            {
+                DeckId = deck_id,
+                DeckName = deckResult.Name,
+                DeckDescription = deckResult.Description,
+            };
+
+            return Result<DeckResponse>.Ok(deckResponse);
+
         }
 
         public Task<Result<List<DeckResponse>>> GetDecksByName(string deckName)
